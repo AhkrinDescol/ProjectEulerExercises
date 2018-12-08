e96202b8aea077ecbe06872ec33343c4d9d55181
Ahkrin 'Descol <amorley525@hotmail.co.uk>
2018-12-08 02:17:15 +0000

    Updated largestPrimeFactor initial value in Exercise003.cpp to initialise to 0

--- Exercise003.cpp
+++ Exercise003.cpp
@@ -3,7 +3,6 @@
 void Exercise003::execute()
 {
 	long limit{600'851'475'143};
-	long largestPrime{0};
-	std::cout << largestPrime << std::endl;
+	long largestPrimeFactor{};
+	std::cout << largestPrimeFactor << std::endl;
 }
-
